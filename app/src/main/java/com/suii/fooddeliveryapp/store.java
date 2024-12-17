21e5a42bd6eef6ea10f8328f14b81fcddb881578
basil03p <basilmalikudy3@gmail.com>
2024-12-18 00:47:55 +0530

    goback added via symbol

--- app/src/main/java/com/suii/fooddeliveryapp/store.java
+++ app/src/main/java/com/suii/fooddeliveryapp/store.java
@@ -1,6 +1,9 @@
 package com.suii.fooddeliveryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class store extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_store);
+        b1 = findViewById(R.id.back);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pk = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(pk);
+            }
+        });
 
     }
 }
